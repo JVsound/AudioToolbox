65413813664ec00af09cde9294073fb4732c3ad6
jvermond <jvermond@CI01366>
2024-04-19 15:45:02 +0200

    Update

--- tbx/audio/+acoustics/@simulation/simulation.m
+++ tbx/audio/+acoustics/@simulation/simulation.m
@@ -6,15 +6,15 @@ classdef simulation
     properties (Access = public)
         f       (:,1) double    {mustBeNonnegative}             = 0                     % Frequency array [Hz]
         encl    (1,1)           {mustBeA(encl,["enclosure.closedbox", ...
-            "enclosure.bassreflex","enclosure.tappedhorn"])}    = enclosure.closedbox;  % Enclosure for this simulation.
+            "enclosure.bassreflex","enclosure.tappedhorn"])}    = enclosure.closedbox   % Enclosure for this simulation.
+        sp      (1,1)           {mustBeA(sp,"speaker.woofer")}  = speaker.woofer        % Speaker for this simulation.
+        nsp     (1,1) double    {mustBeInteger,mustBePositive}  = 1;                    % Number of speakers
     end
     
     properties (Dependent)
         w                                                                               % Frequency array [rad/s]
-    end
-
-    properties (Access=private,Hidden)
-        fint
+        za                                                                              % Equivalent acoustic impedance [Ns/m5]
+        sim2port                                                                        % Simulation of 2-port network 
     end
 
     methods
@@ -28,26 +28,6 @@ classdef simulation
                 obj.f       = logspace(log10(2e1),log10(2e4),1e3);
             end
         end
-        function val = get.f(obj)
-            val = obj.fint;
-        end
-        function obj = set.f(obj,val)
-            %F
-            %
-            %   If the frequency property of the simulation object is
-            %   changed, it must also be entered for all underlying object
-            %   like the enclosures. This is done by the seperate "setf"
-            %   method.
-
-            obj     = setfint(obj,val);
-        end
-        function obj = set.encl(obj,val)
-            %ENCL Set function for encl property
-            %   If an enclosure property is loaded, it has to take over the
-            %   frequency vector of the simulation object.
-
-            obj.encl    = val;
-        end
         function val = get.encl(obj)
             obj.encl.f      = obj.f;
             val             = obj.encl;
@@ -56,12 +36,15 @@ classdef simulation
             %W Frequency in [rad/s]
             val         = obj.f * 2*pi;
         end
-    end
+        function val = get.za(obj)
+            %ZA
 
-    methods (Access=protected,Hidden)
-        function obj = setfint(obj,val)
-            obj.fint        = val;
-            obj.encl.f      = val;
+            val = obj.nsp;
+        end
+        function val = get.sim2port(obj)
+            %SIM2PORT Simulation of 2-port network
+
+            val = 1;
         end
     end
 
